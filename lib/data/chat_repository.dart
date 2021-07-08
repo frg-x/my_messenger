3625a3b99cecf49ad07facb3f5d6b6c2f764c0d5
frg-x <frg_x@mail.ru>
2021-07-08 18:16:00 +0300

    added base functionality

--- lib/data/chat_repository.dart
+++ lib/data/chat_repository.dart
@@ -1,90 +1,78 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:my_messenger/models/user_profile.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:my_messenger/constants.dart';
+import 'package:uuid/uuid.dart';
 
 class ChatRepository {
-  var fireStore = FirebaseFirestore.instance;
-  final auth = FirebaseAuth.instance;
+  // String get _generateNewChannelId {
+  //   return Uuid().v4();
+  // }
 
-  UserProfile userProfile = UserProfile(
-    id: '',
-    firstName: '',
-    lastName: '',
-    phone: '',
-    address: '',
-    email: '',
-    gender: '',
-    maritalStatus: '',
-    preferLanguage: '',
-    avatarUrl: '',
-    channels: [],
-  );
+  // Future<String> getChannelId({required List currentUsers}) async {
+  //   try {
+  //     var channelSnapshot = await FirebaseFirestore.instance.collection('messages').get();
+  //     print(channelSnapshot.docs);
+  //     //int channelDocsLength = await channelSnapshot.length;
+  //     //channelSnapshot.forEach((element) {
+  //     //  print(element.docs);
+  //     //});
+  //     return '12345';
+  //     // if (channelDocsLength != 0) {
+  //     //   return channelSnapshot.docs.first.id;
+  //     // } else {
+  //     //   return _generateNewChannelId;
+  //     // }
+  //   } catch (e) {
+  //     print(e);
+  //     return '';
+  //   }
+  // }
 
-  void addUser(User user) {
-    fireStore.collection('users').doc(user.uid).set({
-      'id': user.uid,
-      'firstName': '',
-      'lastName': '',
-      'phone': '',
-      'address': '',
-      'email': user.email,
-      'gender': '',
-      'maritalStatus': '',
-      'preferLanguage': '',
-      'avatarUrl': '',
-      'channels': [],
+  String getChannelId({required List<String> currentUsers}) {
+    if (currentUsers[0].hashCode <= currentUsers[1].hashCode) {
+      return (currentUsers[1].hashCode - currentUsers[0].hashCode).toString();
+    } else {
+      return (currentUsers[0].hashCode - currentUsers[1].hashCode).toString();
+    }
+  }
+
+  // void createChatCollections({
+  //   required String channelId,
+  //   required String userId,
+  //   required List currentUsers,
+  // }) async {
+  //   try {
+  //     await FirebaseFirestore.instance.collection('messages').add({'users': currentUsers});
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
+
+  Future<dynamic> getFileMetadata(String url) async {
+    return await FirebaseStorage.instance.refFromURL(url).getMetadata();
+  }
+
+  void addMessage({
+    required String content,
+    required int contentType,
+    required String channelId,
+    required List currentUsers,
+    Map<String, String>? metaData,
+  }) async {
+    var timestamp = FieldValue.serverTimestamp();
+
+    await FirebaseFirestore.instance
+        .collection('messages')
+        .doc(channelId)
+        .collection(channelId)
+        .doc()
+        .set({
+      'content': content,
+      'type': contentType,
+      'timestamp': timestamp,
+      'idFrom': currentUsers[0],
+      'idTo': currentUsers[1],
+      'metadata': metaData ?? {},
     });
-  }
-
-  Future<List<UserProfile>> getUsers() async {
-    List<UserProfile> usersList = [];
-    QuerySnapshot querySnapshot = await fireStore
-        .collection('users')
-        .where('id', isNotEqualTo: auth.currentUser!.uid)
-        .get();
-    querySnapshot.docs.forEach((item) {
-      var userData = item.data() as Map<String, dynamic>;
-      UserProfile user = UserProfile.fromJson(userData);
-      usersList.add(user);
-    });
-
-    return usersList;
-  }
-
-  Future<UserProfile> getCurrentUserInfo() async {
-    await fireStore
-        .collection('users')
-        .where('id', isEqualTo: auth.currentUser!.uid)
-        .get()
-        .then((querySnapshot) {
-      userProfile = UserProfile.fromJson(querySnapshot.docs.first.data());
-    });
-
-    return userProfile;
-  }
-
-  Future<void> updateUserInfo(UserProfile newUserinfo) async {
-    userProfile = newUserinfo;
-    await fireStore
-        .collection('users')
-        .doc(newUserinfo.id)
-        .update(newUserinfo.toJson());
-  }
-
-  void logOut() {
-    userProfile = UserProfile(
-      id: '',
-      firstName: '',
-      lastName: '',
-      phone: '',
-      address: '',
-      email: '',
-      gender: '',
-      maritalStatus: '',
-      preferLanguage: '',
-      avatarUrl: '',
-      channels: [],
-    );
-    auth.signOut();
   }
 }
---
+++ lib/data/users_repository.dart
@@ -0,0 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_messenger/models/user_profile.dart';
+
+class UsersRepository {
+  var fireStore = FirebaseFirestore.instance;
+  final auth = FirebaseAuth.instance;
+
+  UserProfile userProfile = UserProfile(
+    id: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+    address: '',
+    email: '',
+    gender: '',
+    maritalStatus: '',
+    preferLanguage: '',
+    avatarUrl: '',
+  );
+
+  void addUser(User user) {
+    fireStore.collection('users').doc(user.uid).set({
+      'id': user.uid,
+      'firstName': '',
+      'lastName': '',
+      'phone': '',
+      'address': '',
+      'email': user.email,
+      'gender': '',
+      'maritalStatus': '',
+      'preferLanguage': '',
+      'avatarUrl': '',
+    });
+  }
+
+  Future<List<UserProfile>> getUsers() async {
+    List<UserProfile> usersList = [];
+    QuerySnapshot querySnapshot =
+        await fireStore.collection('users').where('id', isNotEqualTo: auth.currentUser!.uid).get();
+    querySnapshot.docs.forEach((item) {
+      var userData = item.data() as Map<String, dynamic>;
+      UserProfile user = UserProfile.fromJson(userData);
+      usersList.add(user);
+    });
+
+    return usersList;
+  }
+
+  Future<UserProfile> getCurrentUserInfo() async {
+    await fireStore
+        .collection('users')
+        .where('id', isEqualTo: auth.currentUser!.uid)
+        .get()
+        .then((querySnapshot) {
+      userProfile = UserProfile.fromJson(querySnapshot.docs.first.data());
+    });
+
+    return userProfile;
+  }
+
+  Future<void> updateUserInfo(UserProfile newUserinfo) async {
+    userProfile = newUserinfo;
+    await fireStore.collection('users').doc(newUserinfo.id).update(newUserinfo.toJson());
+  }
+
+  void logOut() {
+    userProfile = UserProfile(
+      id: '',
+      firstName: '',
+      lastName: '',
+      phone: '',
+      address: '',
+      email: '',
+      gender: '',
+      maritalStatus: '',
+      preferLanguage: '',
+      avatarUrl: '',
+    );
+    auth.signOut();
+  }
+}
